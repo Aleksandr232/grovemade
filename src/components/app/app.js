761877f100afe6a7b9fe79b4b62b6a2a7f798c0f
Aleksandrgit config --global user.email  aleksrowing232@gmail.com <aleksrowing232@gmail.com>
2022-01-22 21:02:12 +0300

    grovemade

---
+++ src/components/app/app.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Main from "../main";
+
+import './app.css';
+
+
+export default class App extends Component{
+    render(){
+        return(
+            <Router>
+                <div>
+                    <Routes>
+                    <Route exact path='/' element={<Main/>}></Route>
+                    </Routes>
+                </div>
+            </Router>
+        )
+    }
+} 
---
+++ src/components/main/header/header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import vector from './Vector.png';
+import vector1 from './vector1.png';
+import g from './g.png';
+import r from './r.png';
+import o from './o.png';
+import v from './m.png';
+import e from './e.png';
+import m from './v.png';
+import a from './a.png';
+import d from './d.png';
+import ee from './ee.png';
+
+
+const Header =()=>{
+    return(
+        <div className="header" >
+            <Link className="header_s" to=''>SHOP</Link>
+            <Link className="header_e" to=''>EXPLORE</Link>
+            <div className="grovemade">
+                <img src={vector1} alt="" />
+                <img src={vector} alt="" className="vector" />
+                <img src={g} alt=""  className="g"/>
+                <img src={r} alt="" className="r" />
+                <img src={o} alt="" className="o" />
+                <img src={v} alt="" />
+                <img src={e} alt="" />
+                <img src={m} alt="" />
+                <img src={a} alt="" />
+                <img src={d} alt="" />
+                <img src={ee} alt="" />
+
+            </div>
+        </div>
+    )
+}
+
+export default Header;
