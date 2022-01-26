8c89eef2c5dcae5f55ed63caf104a81deb563ee9
Aleksandrgit config --global user.email  aleksrowing232@gmail.com <aleksrowing232@gmail.com>
2022-01-26 13:05:24 +0300

    grid

--- src/components/main/desing/desing.js
+++ src/components/main/desing/desing.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+import deskpads from './deskpads.png';
+import laptop from './laptop.png';
+
 const Desing=()=>{
     return(
             <section className="desing">
@@ -8,6 +11,18 @@ const Desing=()=>{
                     <div className="sub_title_desing">Build your dream workspace, so you can get your best work done.</div>
                     <div className="getstarted">GET STARTED</div>
                 </div>
+                <div className="grid_desing">
+                    <div className="deskpads">
+                        <img src={deskpads} alt="" className="img_deskpads" />
+                        <div className="title_pads">Desk Pads</div>
+                        <div className="learnmore">LEARN MORE</div>
+                    </div>
+                    <div className="deskpads">
+                        <img src={laptop} alt="" className="img_laptop" />
+                        <div className="title_pads">Laptop Stands</div>
+                        <div className="learnmore">LEARN MORE</div>
+                    </div>
+                </div> 
             </section>
     )
 }
